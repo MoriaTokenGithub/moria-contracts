597a5370756e4c408687c07578ee092af8616132
James Cunningham <james.cunningham@inwage.com>
2018-01-17 12:23:03 +0000

    Fix contract initialiser

--- src/contracts/MoriaToken.sol
+++ src/contracts/MoriaToken.sol
@@ -1,10 +1,9 @@
 import "./HumanStandardToken.sol";
 import "./DividendToken.sol";
-import "./LibQatt.sol";
 
 pragma solidity ^0.4.18;
 
-contract MoriaToken is HumanStandardToken, DividendToken {
+contract MoriaToken is DividendToken {
 
   function MoriaToken (uint256 _initialAmount,
                        uint8 _decimalUnits
@@ -13,6 +12,7 @@ contract MoriaToken is HumanStandardToken, DividendToken {
                         "Moria dividend token",
                         _decimalUnits,
                         "MORIA"
-                      ) public {                        
+                      ) public {
+                        holdings[msg.sender][0] = _initialAmount;
                       }  
 }
